5e571f9ed011ca4ac58800d61bc170a93d6f9749
tzlingard <63806844+tzlingard@users.noreply.github.com>
2021-09-28 22:49:04 -0400

    added trash icon

--- components/Note.js
+++ components/Note.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Note = (props) => {
-
     return(
         <View style={ styles.item}>
             <Text>{props.text}</Text>
+                <FontAwesomeIcon icon = { faTrashAlt } />
         </View>
     )
 }
@@ -18,8 +19,10 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
         marginBottom: 20,
-}
+        marginHorizontal: 20,
+    }
 });
 
 export default Note;
